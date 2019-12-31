f5d45f55bfef3caf541fa7e22aeaa3fea8e7b5a7
Julian Nicholls <juliannicholls29@gmail.com>
2019-12-31 15:59:00 +0000

    Fix handling of numbers for padding.
    Make handling of unspecified width explicit, it was always sane but I
    think this is better.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 function padding(str, width, fillChar) {
   const padBytes = width - str.length;
-  let padding  = '';
+  let padding = '';
 
-  for(let i = 0; i < padBytes; ++i) {
+  for (let i = 0; i < padBytes; ++i) {
     padding += fillChar;
   }
 
   return padding;
 }
 
-exports.ljust = function(str, width, fillChar=' ') {
+exports.ljust = function(str, width = 0, fillChar = ' ') {
+  if (typeof str === 'number') str = str.toString();
+
   return str + padding(str, width, fillChar);
 };
 
-exports.rjust = function(str, width, fillChar=' ') {
+exports.rjust = function(str, width = 0, fillChar = ' ') {
+  if (typeof str === 'number') str = str.toString();
+
   return padding(str, width, fillChar) + str;
 };
